2b571d65c6306d9b6a295894377603bffa5fdfb0
Luke Lawton <llawton25@student.dimanregional.org>
2022-12-02 17:38:55 -0500

    made a settings page, with nested tree layout

---
+++ js/settings.js
@@ -0,0 +1,29 @@
+const topnav = document.querySelector('#top-navbar'),
+      container = document.querySelector('.container'),
+      setting_wrapper = document.querySelector('.settings_wrapper')
+
+//sets the position of the settings div to be directly below the navbar
+const settings_wrapper_height = () => {
+    let topnavrect = topnav.getBoundingClientRect()
+    setting_wrapper.style.top = `${topnavrect.bottom}px`
+}
+
+window.onload = () => {
+    settings_wrapper_height()
+    window.addEventListener('resize', (e) => settings_wrapper_height(e))
+    const tree_branches = document.querySelectorAll('.tree_branch')
+    tree_branches.forEach(branch => {
+        branch.addEventListener('click', (e) => {
+            branch.parentElement.childNodes.forEach(leaf => {
+                if (leaf.tagName == 'UL') {
+                    leaf.classList.toggle('active')
+                    leaf.parentElement.childNodes[0].classList.toggle('active')
+                    // console.log(leaf);
+                    // leaf.classList.toggle('expanded');
+                }
+            });
+        })
+    });
+}
+
+
